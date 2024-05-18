02e5ef5ab72b998726f63b1e8cc1130d1f5ab4ed
roombawulf <prydax997@gmail.com>
2024-05-18 15:01:06 +0100

    clean up demo

--- components/canvas/Experience.js
+++ components/canvas/Experience.js
@@ -3,19 +3,20 @@ import { OrbitControls, Environment } from "@react-three/drei";
 import Models from "./Models";
 import Water from "./Water";
 
-function Experience(){
-
+function Experience() {
     return (
-        <div className="canvas-container">
-            <Canvas camera={{position: [0,2,2]}} shadows='soft'>
-                <color attach="background" args={['#071416']} />
-                <Environment files={"industrial_sunset_puresky_1k.hdr"} />
-                <ambientLight intensity={0.1} color={'white'}/>
-                <Models />
-                <Water />
-                <OrbitControls />
-            </Canvas>
-        </div>
-    )
+        <>
+            <div className="canvas-container">
+                <Canvas camera={{ position: [0, 2, 2] }} shadows="soft">
+                    <Environment files={"industrial_sunset_puresky_1k.hdr"} />
+                    <Models />
+                    <Water />
+                    <OrbitControls />
+                    <ambientLight intensity={0.1} color={"white"} />
+                    <color attach="background" args={["#071416"]} />
+                </Canvas>
+            </div>
+        </>
+    );
 }
-export default Experience
+export default Experience;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Experience from "../components/canvas/Experience"
+import { Leva } from "leva";
+import Experience from "../components/canvas/Experience";
 
 function HomePage() {
     return (
-      <Experience />
-    )
-  }
+        <>
+            <Leva collapsed={true} />
+            <Experience />
+        </>
+    );
+}
 
-  export default HomePage
-  
+export default HomePage;
